9a359560875ce407ee0ca4a66398df5efb5a5b4d
Stanislas Ormières <stan@stormier.ninja>
2019-05-29 19:30:49 +0200

    :loud_sound: Add logger and basic logs

--- src/app.js
+++ src/app.js
@@ -1,24 +1,33 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import fileupload from 'express-fileupload'
+import morgan from 'morgan'
 
+import { loggerStream } from './util/logger'
 import npmVersion from '../package.json'
 import mime from 'mime'
 import convertToPdf from './convert-to-pdf'
 
 const app = express()
-
 app.disable('x-powered-by')
 
+const contentLimit = '20mb'
+const fileSizeLimit = 50 * 1024 * 1024
+
 export const apiPrefix = '/api/v1'
+
+const formatAsNginx =
+':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :response-time'
+
+app.use(morgan(formatAsNginx, { stream: loggerStream }))
 
 app.get(`${apiPrefix}/version`, function (req, res) {
   res.send(npmVersion.version)
 })
 
-app.use(bodyParser.json({ limit: '20mb' }))
-app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }))
-app.use(fileupload({ limits: { fileSize: 50 * 1024 * 1024 } }))
+app.use(bodyParser.json({ limit: contentLimit }))
+app.use(bodyParser.urlencoded({ limit: contentLimit, extended: false }))
+app.use(fileupload({ limits: { fileSize: fileSizeLimit } }))
 
 app.post(`${apiPrefix}/html2pdf`, async (req, res) => {
   const { html, filename } = req.body
@@ -32,7 +41,7 @@ app.post(`${apiPrefix}/html2pdf`, async (req, res) => {
     })
     res.send(buffer)
   } catch (error) {
-    res.json({
+    res.status(error.httpStatusCode || 500).json({
       error: error.message,
     })
   }
